932ed9f7737f86bb6cf2a04fa45481b38e8de9f7
QuantumAstronomer <t.essen@hotmail.com>
2021-06-23 13:55:07 +0200

    Added fillghost and pretty print functionality

--- HD/Mesh2D/extendedarray.py
+++ HD/Mesh2D/extendedarray.py
@@ -118,6 +118,240 @@ class ExtendedArray(np.ndarray):
         lap5 = self.laplacian5(nc = nc, nbuf = nbuf)
         lap9 = (self.ijshift(-1, -1, nc = nc, nbuf = nbuf) + self.ijshift(1, 1, nc = nc, nbuf = nbuf) +
                 self.ijshift(-1, 1, nc = nc, nbuf = nbuf) + self.ijshift(1, -1, nc = nc, nbuf = nbuf) -
-                2 * self.valid(nc = nc, nbuf = nbuf)) / (self.g.dx * self.g.dy)
+                4 * self.valid(nc = nc, nbuf = nbuf)) / (self.g.dx * self.g.dy)
 
-        return lap5 + .5 * lap9
+        return .5 * lap5 + .25 * lap9
+
+    def norm(self, nc = 0):
+        """
+        Calculate the norm of the quantity (with index nc) defined on the grid
+        within the domain's valid data region.
+        """
+
+        c = len(self.shape)
+        if c == 2:
+            return np.sqrt(self.g.dx * self.g.dy *
+                           np.sum((self[self.g.ilo : self.g.ihi + 1, self.g.jlo : self.g.jhi + 1]**2).flat))
+        elif c == 3:
+            return np.sqrt(self.g.dx * self.g.dy *
+                           np.sum((self[self.g.ilo : self.g.ihi + 1, self.g.jlo : self.g.jhi + 1, nc]**2).flat))
+
+    def copy(self):
+        """
+        Make a copy of the array defined on the same grid in order to modify
+        it without changing anything in the original array/grid
+        """
+
+        return ExtendedArray(np.asarray(self).copy(), grid = self.g)
+
+    def symmetric(self, nodal = False, atol = 1.e-14, asym = False):
+        """
+        Check whether the data on the grid is left-right symmetric within some
+        absolute tolerance given by atol.
+        nodal mode is used for node-centered datastructures.
+        """
+
+        ## Prefactor to convert from symmetric to asymmetric test
+        s = 1
+        if asym:
+            s = -1
+
+        if not nodal:
+            L = self[self.g.ilo : self.g.ilo + self.g.nx // 2,
+                     self.g.jlo : self.g.jhi + 1]
+            R = self[self.g.ilo + self.g.nx // 2 : self.g.ihi + 1
+                     self.g.jlo : self.g.jhi + 1]
+        elif nodal:
+            L = self[self.g.ilo : self.g.ilo + self.g.nx // 2 + 1,
+                     self.g.jlo : self.g.jhi + 1]
+            R = self[self.g.ilo + self.g.nx // 2 : self.g.ihi + 2,
+                     self.g.jlo : self.g.jhi + 1]
+        e = np.abs(L - s * np.flipud(R)).max()
+        return e < atol
+
+    def asymmetric(self, nodal = False, atol = 1.e-14):
+        """
+        Check wheter data on the grid is left-right asymmetric to a given
+        tolerance. For example check for sign flips, etc.
+        nodal mode is used for node-centered datastructures.
+        """
+
+        return self.symmetric(nodal = nodal, atol = atol, asym = True)
+
+    def fillghost(self, nc = 0, bc = None):
+        """
+        Using a boundary condition object, fill in the ghost cells
+        to satisfy these boundary conditions.
+        The BC object is used to tell us which condition to apply
+        on each boundary of the grid.
+
+        Since there is only a single grid, every boundary is physically
+        there, except when using periodic boundary conditions.
+        To fill in the Neumann and Dirichlet conditions, we reuse the
+        outflow and reflect-odd boundaries.
+        """
+
+        ## Lower x-boundary
+        if bc.xlb in ["outflow", "neumann"]:
+            if bc.xl_value is None:
+                for i in range(self.g.ilo):
+                    self[i, :, nc] = self[self.g.ilo, :, nc]
+            else:
+                self[self.g.ilo - 1, :, nc] = (self[self.g.ilo, :, nc] -
+                                               self.g.dx * bc.xl_value[:])
+
+        elif bc.xlb == "reflect-even":
+            for i in range(self.g.ilo):
+                self[i, :, nc] = self[2 * self.g.ng - i - 1, :, nc]
+
+        elif bc.xlb in ["reflect-odd", "dirichlet"]:
+            if bc.xl_value is None:
+                for i in range(self.g.ilo):
+                    self[i, :, nc] = - self[2 * self.g.ng - i - 1, :, nc]
+            else:
+                self[self.g.ilo - 1, :, nc] = (2 * bc.xl_value[:] -
+                                               self[self.g.ilo, :, nc])
+
+        elif bc.xlb == "periodic":
+            for i in range(self.g.ilo):
+                self[i, :, nc] = self[self.g.ihi - self.g.ng + i + 1, :, nc]
+
+
+        ## Upper x-boundary
+        if bc.xrb in ["outflow", "neumann"]:
+            if bc.xr_value is None:
+                for i in range(self.g.ihi + 1, self.g.nx + 2 * self.g.ng):
+                    self[i, :, nc] = self[self.g.ihi, :, nc]
+            else:
+                self[self.g.ihi + 1, :, nc] = (self[self.g.ihi, :, nc] -
+                                               self.g.dx * bc.xr_value)
+
+        elif bc.xrb == "reflect-even":
+            for i in range(self.g.ng):
+                self[self.g.ihi + i + 1, :, nc] = self[self.g.ihi - i, :, nc]
+
+        elif bc.xrb in ["reflect-odd", 'dirichlet']:
+            if bc.xr_value is None:
+                for i in range(self.g.ng):
+                    self[self.g.ihi + i + 1, :, nc] = - self[self.g.ihi - i, :, nc]
+
+            else:
+                self[self.g.ihi + 1, :, nc] = (2 * bc.xr_value[:] -
+                                               self[self.g.ihi, :, nc])
+
+        elif bc.xrb == "periodic":
+            for i in range(self.g.ihi + 1, self.g.nx + 2 * self.g.ng):
+                self[i, :, nc] = self[i - self.g.ihi - 1 + self.g.ng, :, nc]
+
+        ## Lower y-boundary
+        if bc.ylb in ["outflow", "neumann"]:
+            if bc.yl_value is None:
+                for j in range(self.g.jlo):
+                    self[:, j, nc] = self[:, self.g.jlo, nc]
+            else:
+                self[:, self.g.jlo, nc] = (self[:, self.g.jlo, nc] -
+                                           self.g.dy * bc.yl_value[:])
+
+        elif bc.ylb == "reflect-even":
+            for j in range(self.g.jlo):
+                self[:, j, nc] = self[:, 2 * self.g.ng - j -1, nc]
+
+        elif bc.ylb in ["reflect-odd", "dirichlet"]:
+            if bc.yl_value is None:
+                for j in range(self.g.jlo):
+                    self[:, j, nc] = self[:, 2 * self.g.ng - j -1, nc]
+            else:
+                self[:, self.g.jlo - 1, nc] = (2 * bc.yl_value[:] -
+                                               self[:, self.g.jlo, nc])
+
+        elif bc.ylb == "periodic":
+            for j in range(self.g.jlo):
+                self[:, j, nc] = self[:, self.g.jhi - self.g.ng + j + 1, nc]
+
+        ## Upper y-boundary
+        if bc.yrb in ["outflow", "neumann"]:
+            if bc.yr_value is None:
+                for j in range(self.g.jhi + 1, self.g.ny + 2 * self.g.ng):
+                    self[:, j, nc] = self[:, self.g.jhi, nc]
+            else:
+                self[:, self.g.jhi + 1, nc] = (self[:, self.g.jhi, nc] +
+                                               self.g.dy * bc.yr_value[:])
+
+        elif bc.yrb == "reflect-even":
+            for j in range(self.g.ng):
+                self[:, self.g.jhi + j + 1, nc] = self[:, self.g.jhi - j, nc]
+
+        elif bc.yrb in ["reflect-odd", "dirichlet"]:
+            if bc.yr_value is None:
+                for j in range(self.g.ng):
+                    self[:, self.g.jhi + j +1, nc] = - self[:, self,g.jhi - j, nc]
+
+            else:
+                self[:, self.g.jhi + 1, nc] = (2 * bc.yr_value[:] -
+                                               self[:, self.g.jhi, nc])
+
+        elif bc.yrb == "periodic":
+            for j in range(self.g.jhi + 1, self.g.ny + 2 * self.g.ng):
+                self[:, j, nc] = self[:, j - self.g.jhi - 1 + self.g.ng, nc]
+
+
+    def pprint(self, nc = 0, fmt = None, show_ghost = True):
+        """
+        Print out a dataset on the screen with ghost cells in
+        a different color to make it visually easy to identify them.
+
+        CAUTION: only use on small datasets, large datasets take a
+        long time to print and will be replaced with ... partially as
+        per the typical numpy.ndarray print behavior.
+        """
+
+        if fmt is None:
+            if self.dtype == np.int:
+                fmt = "%4d"
+            elif self.dtype == np.float64:
+                fmt = "8.5g%""
+            else:
+                raise ValueError("ERROR: dtype not supported")
+
+        if show_ghost:
+            ilo = 0
+            ihi = self.g.qx - 1
+            jlo = 0
+            jhi = self.g.qy - 1
+        else:
+            ilo = self.g.ilo
+            ihi = self.g.ihi
+            jlo = self.g.jlo
+            jhi = self.g.jhi
+
+        ## Reverse order of j, i.e. print in descending order, so it looks like
+        ## a proper grid, with y increasing with height.
+        for j in reversed(range(jlo, jhi + 1)):
+            for i in range(ilo, ihi + 1):
+
+                if (j < self.g.jlo or j > self.g.jhi or
+                    i < self.g.ilo or i > self.g.ihi):
+                    gc = 1
+                else:
+                    gc = 0
+
+                if self.c == 2:
+                    val = self[i, j]
+
+                else:
+                    try:
+                        val = self[i, j, nc]
+                    except IndexError:
+                        val = self[i, j]
+
+                if gc:
+                    print("\033[31m" + fmt % (val) + "\033[0m", end = "")
+                else:
+                    print(fmt % (val), end = "")
+
+        leg = """
+         ^ y
+         |
+         +---> x
+        """
+        print(leg)
